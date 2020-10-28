d80253f3d048aa6c4540ff0d4e393a08b1c9fb40
HabaOleh <gabikoleh25@gmail.com>
2020-10-28 21:28:14 +0200

    JS Task 1

---
+++ Homework/05/js.js
@@ -0,0 +1,209 @@
+// //1
+// let congrats ="hello";
+// console.log(congrats);
+// alert(congrats);
+// document.write(congrats);
+// let university="owu";
+// console.log(university);
+// alert(university);
+// document.write(university);
+// let directory ="com";
+// console.log(directory);
+// alert(directory);
+// document.write(directory);
+// let ukr = "ua";
+// console.log(ukr);
+// alert(ukr);
+// document.write(ukr);
+// let a=1;
+// console.log(a);
+// alert(a);
+// document.write(a);
+// let b=10;
+// console.log(b);
+// alert(b);
+// document.write(b);
+// let c=-999;
+// console.log(c);
+// alert(c);
+// document.write(c);
+// let d=123;
+// console.log(d);
+// alert(d);
+// document.write(d);
+// let pi=3.14;
+// console.log(pi);
+// alert(pi);
+// document.write(pi);
+// let expo = 2.7;
+// console.log(expo);
+// alert(expo);
+// document.write(expo);
+// let f=16;
+// console.log(f);
+// alert(f);
+// document.write(f);
+// let tr = true;
+// console.log(tr);
+// alert(tr);
+// document.write(tr);
+// let fl =false;
+// console.log(fl);
+// alert(fl);
+// document.write(fl);
+
+// //2
+// congrats= "hi";
+// console.log(congrats);
+// alert(congrats);
+// document.write(congrats);
+// university="nltu";
+// console.log(university);
+// alert(university);
+// document.write(university);
+// directory="lesson1:";
+// console.log(directory);
+// alert(directory);
+// document.write(directory);
+// ukr="Ukrainian";
+// console.log(ukr);
+// alert(ukr);
+// document.write(ukr);
+// a=100;
+// b=56;
+// c=354.45;
+// d=555;
+// console.log(d);
+// alert(d);
+// document.write(d);
+// console.log(b);
+// alert(b);
+// document.write(b);
+// console.log(c);
+// alert(c);
+// document.write(c);
+// console.log(a);
+// alert(a);
+// document.write(a);
+// pi=3;
+// console.log(pi);
+// alert(pi);
+// document.write(pi);
+// expo=10;
+// console.log(expo);
+// alert(expo);
+// document.write(expo);
+// f=0;
+// document.write(f);
+// console.log(f);
+// alert(f);
+// tr=false;
+// fl=true;
+// console.log(tr);
+// alert(tr);
+// document.write(tr);
+// console.log(fl);
+// alert(fl);
+// document.write(fl);
+
+// //3
+// const str1="ok";
+// const str2="no";
+// const str3="good";
+// console.log(str1);
+// alert(str1);
+// document.write(str1);
+// console.log(str2);
+// alert(str2);
+// document.write(str2);
+// console.log(str3);
+// alert(str3);
+// document.write(str3);
+
+// //4 //5 //6
+// let FirstName = prompt("What is your first Name?");
+// let LastName = prompt("What is your last Name?");
+// let FatherName = prompt("What is your Father Name?");
+// console.log(FirstName);
+// alert(FirstName);
+// document.write(FirstName);
+// console.log(LastName);
+// alert(LastName);
+// document.write(LastName);
+// console.log(FatherName);
+// alert(FatherName);
+// document.write(FatherName);
+// let person=FirstName+LastName+FatherName;
+// console.log(person);
+// alert(person);
+// document.write(person);
+
+// //7
+// let num1 = prompt("Enter number");
+// let num2 = prompt("Enter number");
+// let num3 = prompt("Enter number");
+// console.log(+num1);
+// console.log(+num2);
+// console.log(+num3);
+
+// //8
+// let num1 = parseInt(prompt("Enter number"));
+// let num2 = parseInt(prompt("Enter number"));
+// let num3 = parseInt(prompt("Enter number"));
+// let num4 = parseInt(prompt("Enter number"));
+// let result = num1+num2+num3+num4;
+// console.log(result);
+
+////9
+// let num1 = parseFloat(prompt("Enter float number"));
+// let num2 = parseFloat(prompt("Enter float number"));
+// let num3 = parseFloat(prompt("Enter float number"));
+// let result = num1+num2+num3;
+// console.log(result);
+
+// //10
+// let num1 = Math.round(parseFloat(prompt("Enter float number")));
+// let num2 = Math.round(parseFloat(prompt("Enter float number")));
+// let num3 = Math.round(parseFloat(prompt("Enter float number")));
+// let result = num1+num2+num3;
+// console.log(result);
+
+// //11
+// let num1 = +prompt("Enter float number");
+// let num2 = +prompt("Enter float number");
+// let result=Math.pow(num1,num2);
+// console.log(result);
+
+// //12
+// let a = 100;
+// let b = '100';
+// let c = true;
+// let d = undefined;
+// console.log(typeof(a));
+// console.log(typeof (b));
+// console.log(typeof (c));
+// console.log(typeof (d));
+
+//13
+let a=5<6;
+console.log(a);
+let b=5>6;
+console.log(b);
+let c=5>=6;
+console.log(c);
+let d=5===6;
+console.log(d);
+let e=10==10;
+console.log(e);
+let f=10===10;
+console.log(f);
+let g=10<10;
+console.log(g);
+let i=10>10;
+console.log(i);
+let h=10!=10;
+console.log(h);
+let t=123==='123';
+console.log(t);
+let m=123=='123';
+console.log(m);
